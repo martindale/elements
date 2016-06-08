f6d060270f7b003e1e72b3437269427cc39b4ba9
Warren Togami <wtogami@gmail.com>
2016-06-07 22:26:25 -0700

    Revert "Rename more strings to Liquid"
    This reverts commit 40fa759bf4d7d671c7c6b2660104646578e3e673.

--- src/rpc/server.cpp
+++ src/rpc/server.cpp
@@ -254,11 +254,11 @@ UniValue stop(const UniValue& params, bool fHelp)
     if (fHelp || params.size() > 1)
         throw runtime_error(
             "stop\n"
-            "\nStop Liquid server.");
+            "\nStop Bitcoin server.");
     // Event loop will exit after current HTTP requests have been handled, so
     // this reply will get back to the client.
     StartShutdown();
-    return "Liquid Cloud server stopping";
+    return "Bitcoin server stopping";
 }
 
 /**
